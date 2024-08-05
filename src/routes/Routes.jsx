188f0688fabb2be8adc0bb31b873af05b9fe860e
Keeity Braga Collodel <keeity@hotmail.com>
2024-08-05 00:56:52 -0300

    corrigido o erro, fazendo com que seja ajustado o problema de aceitar qualquer senha

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,42 +6,13 @@ import DashboardPage from '../pages/Dashboard';
 import SpotRegistrationPage from '../pages/SpotRegistration';
 import SpotsPage from '../pages/Spots';
 import PrivateRoute from './PrivateRoute';
-// Endpoints:
-// http://localhost:3000/usuarios
-// http://localhost:3000/spots
 
 
 function RoutesComponent() {
 
-    // const { isAuthenticated } = useAuth()
-    // const isAuthenticated = true    //muda para true ou false para teste. Fazer o mesmo no app.jsx
-
-    //        function loginRedirect(component) {
-    //     if (isAuthenticated) {
-    //         // return <Navigate to='/home' replace />
-    //     }
-    //      return component
-    // }
 
     return (
-        // isAuthenticated
-        // <Routes>
-        //     <Route path='/login' element={loginRedirect(<SigninPage/>)} />
-        //     <Route path='/cadastro' element={loginRedirect(<SignupPage/>)} />
-        //     <Route path='/' element={loginRedirect(<DashboardPage/>)} />
-        //     <Route path='/dashboard' Component={DashboardPage} />
-        //     <Route path='/locais' Component={SpotsPage} />
-        //     <Route path='/local' Component={SpotRegistrationPage} />
-        //     <Route path='*' element={<Navigate replace to='/dashboard' />} />
-        //     {/* {isAuthenticated ? (
-              
-        //     ) : (
-           
-        //     )} */}
-
-        //     {/* /nao-existente */}
-        //     {/* <Route path='*' Component={App} /> */}
-        // </Routes>
+  
         <Routes>
         <Route path='/login' element={<SigninPage />} />
         <Route path='/cadastro' element={<SignupPage />} />
