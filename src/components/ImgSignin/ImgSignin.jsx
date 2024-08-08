87f4ed8af864cb2f4e250ca72946df1689a8bf05
Keeity Braga Collodel <keeity@hotmail.com>
2024-08-07 23:40:08 -0300

    finalizada a pagina signup

--- src/components/ImgSignin/ImgSignin.jsx
+++ src/components/ImgSignin/ImgSignin.jsx
@@ -17,10 +17,10 @@ function ImgSignin() {
     <h2 className="h2imgsignin">Experiências que transformam,<br></br>       Lugares que inspiram...</h2>
     <h2 className="h2imgsignin" left="20">Experiências que transformam,<br></br>       Lugares que inspiram...</h2>
     </div>
-    <img    type="image/jpeg"  src="/public/imgsignin.jpeg"
+    {/* <img    type="image/jpeg"  src="/public/imgsignin.jpeg"
           // <link rel="icon" href="/logotrip.png" />    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
  
-    ></img>
+    ></img> */}
 
     </div>
   );
---
+++ src/components/ImgSignup/ImgSignup.jsx
@@ -0,0 +1,21 @@
+import "./ImgSignup.css";
+
+function ImgSignup() {
+
+  return (
+    <div className="imgSignup">
+    
+  
+          {/* <img className="logotripflowformsignup" src="/public/logotripflow.png" alt="Logo" /> */}
+    <div className="h2Signup">
+    <h2 className="h2imgSignup" left="20">Experiências únicas em destinos inesquecíveis...</h2>
+    </div>
+    {/* <img className="iSignup"   type="image/jpg"  src="/public/imgsignup2.jpg"
+ 
+    ></img> */}
+      <img className="logotripflowformsignupp" src="/public/logotripflowe.png" alt="Logo" />
+    </div>
+  );
+}
+
+export default ImgSignup;
